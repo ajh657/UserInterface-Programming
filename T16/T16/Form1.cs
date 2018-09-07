8f0d27397d2ab8458c3318eb0622c37550e04963
Arttu Höylä <aahoo01@gmail.com>
2018-09-07 13:29:03 +0300

    Continued

--- T16/T16/Form1.cs
+++ T16/T16/Form1.cs
@@ -16,5 +16,66 @@ namespace T16
         {
             InitializeComponent();
         }
+        
+        private void CreatePicturebox(string name,int x,int y,Image image)
+        {
+            PictureBox picture = new PictureBox
+            {
+                Name = name,
+                Size = new Size(25, 25),
+                Location = new Point(x, y),
+                Image = image,
+                
+                
+            };
+            this.Controls.Add(picture);
+            picture.Click += new EventHandler(pictureBox_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool Loop = true;
+            int x = 25;
+            int y = 25;
+            int xname = 0;
+            int yname = 0;
+            string name;
+            
+
+            while (Loop)
+            {
+                for (int i = 0; i <= 16; i++)
+                {
+
+                    name = xname.ToString() + yname.ToString();
+
+                    CreatePicturebox(name, x, y, Properties.Resources.Icon);
+
+                    x = x + 25;
+                    xname += 1;
+                    //yname += 1;
+                }
+
+                if (yname != 16)
+                {
+                    yname++;
+                    y += 25;
+                    x = 25;
+                }
+                else
+                {
+                    Loop = false;
+                }
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+
+            PictureBox pictureBox = sender as PictureBox;
+
+            label1.Text = pictureBox.Name;
+        }
     }
 }
